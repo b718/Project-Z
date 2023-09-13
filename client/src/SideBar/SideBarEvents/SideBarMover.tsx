c223d3f50174142b84a952f5c6e3bd30b7d2815c
whiffy <bryanzhao168@yahoo.ca>
2023-09-12 17:36:41 -0700

    Sending to dev to test.

--- client/src/SideBar/SideBarEvents/SideBarMover.tsx
+++ client/src/SideBar/SideBarEvents/SideBarMover.tsx
@@ -27,7 +27,7 @@ const SideBarMover = () => {
       let latlngexpression = sideBarMoveContext.sideBarMoveLocation;
       let array = latlngexpression as Array<number>;
       if (marker[0][0] == array[0] && marker[0][1] == array[1]) {
-        // console.log(marker[1]);
+        console.log(marker[1]);
         map.openPopup(marker[1].current);
         map.panTo(sideBarMoveContext.sideBarMoveLocation);
         map.flyTo(sideBarMoveContext.sideBarMoveLocation, 18);
