bc271ce4033490bce1a9600beb8210f6a4a5500e
whiffy <bryanzhao168@yahoo.ca>
2023-09-14 17:59:44 -0700

    Testing another change.

--- client/src/SideBar/SideBarEvents/SideBarMover.tsx
+++ client/src/SideBar/SideBarEvents/SideBarMover.tsx
@@ -26,9 +26,9 @@ const SideBarMover = () => {
       console.log(marker[1]);
       if (marker[0][0] == array[0] && marker[0][1] == array[1]) {
         // console.log(marker[1]);
-        map.openPopup(marker[1].current);
-        // map.panTo(sideBarMoveContext.sideBarMoveLocation);
-        // map.flyTo(sideBarMoveContext.sideBarMoveLocation, 18);
+        // map.openPopup(marker[1].current);
+        map.panTo(sideBarMoveContext.sideBarMoveLocation);
+        map.flyTo(sideBarMoveContext.sideBarMoveLocation, 18);
       }
     });
   };
