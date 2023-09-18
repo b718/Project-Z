24fbe28b70fad0496a7f27dae1e173d07177abde
Bryan Zhao <bryan718@students.cs.ubc.ca>
2023-09-17 19:39:52 -0700

    Added IG logo.

--- client/src/Map/Legend-BL/LegendBL.tsx
+++ client/src/Map/Legend-BL/LegendBL.tsx
@@ -2,11 +2,20 @@ import React, { useEffect } from "react";
 import { useMap } from "react-leaflet";
 import L, { Control } from "leaflet";
 import "./LegendBL.css";
+import igLogo from "../../Images/ig_logo.png";
+import { Flex, Image, Text } from "@mantine/core";
 
 const LegendBL = () => {
   return (
     <div className="legendbl-div-for-click">
-      When Add is On, Single Click to spawn a marker.
+      <Flex justify={"center"} align={"center"}>
+        <a href="https://www.instagram.com/pinnitubc/" target="_blank">
+          <Image src={igLogo} width={"20px"}></Image>
+        </a>
+        <Text className="legend-bl-text-ig" style={{ marginLeft: "0.3rem" }}>
+          Follow Us @pinnitubc!
+        </Text>
+      </Flex>
     </div>
   );
 };
