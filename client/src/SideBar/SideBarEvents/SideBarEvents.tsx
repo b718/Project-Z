8bf67fb1998d92e5d4bf6b0f27ab1773e60c6ee0
Bryan Zhao <bryanzhao168@yahoo.ca>
2024-11-25 17:44:56 -0800

    Update SideBarEvents.tsx

--- client/src/SideBar/SideBarEvents/SideBarEvents.tsx
+++ client/src/SideBar/SideBarEvents/SideBarEvents.tsx
@@ -90,7 +90,6 @@ const SideBarEvents = () => {
   return (
     <>
       <Text className="side-bar-events-main-title">Upcoming Events</Text>
-      <Text>Testing AWS</Text>
       <Flex
         justify={"center"}
         align={"center"}
